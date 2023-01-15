b1fdafc5209bfa8fa8a14213b35b54aa142fdb17
XeniaLS13 <kseniavagabova@mail.ru>
2023-01-16 02:34:35 +0300

    Коды по заданиям, файл readme

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length)
+  {
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArr(int[] collection)
+{
+  int count = collection.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(collection[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arr = new int[cube+1];
+Cube(arr);
+PrintArr(arr);
